d847638b7fadeea671974c50e9105630e86d60a8
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-01-19 15:01:46 +0700

    lab12_1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,29 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[])
+{
+
+double avg = 0,max =A[0],min = A[0], sum = 0, SD,GM,HM,multiplysum =1,hmsum;
+for(int i =0; i<N; i++)
+{   
+    hmsum += 1.0/A[i];
+    multiplysum *= A[i];
+    avg += A[i];
+    if(A[i] > max) max = A[i];
+    if(A[i] < min) min = A[i];
+    sum += pow(A[i],2);
+}
+avg /= N;
+HM = N/hmsum;
+SD = sqrt(sum/N-pow(avg,2));
+
+GM = pow(multiplysum,1.0/N);
+B[0] = avg;
+B[1] = SD;
+B[2] = GM;
+B[3] = HM;
+B[4] = max;
+B[5] = min;
+}
